0dea04f0aca8f8b245303d7824cec4f48ba6c479
gaylonalfano <gaylon.alfano@gmail.com>
2018-12-29 15:58:09 -0600

    Added a CreateView view to allow users to create posts directly through the site.

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,16 @@
 # blog/models.py
 from django.db import models
+from django.urls import reverse
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=200)
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE,)
+    title = models.CharField(max_length=200)
     body = models.TextField()
 
     def __str__(self):
         """This improves readability inside admin panel"""
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[str(self.id)])
