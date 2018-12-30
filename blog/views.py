9ae541bc69526e3bd75ff4530dd31565ad7c5ed8
gaylonalfano <gaylon.alfano@gmail.com>
2018-12-29 22:33:34 -0600

    Added updating and deleting blog post functionality: post_edit, post_delete

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 # blog/views.py
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from . models import Post
+from django.urls import reverse_lazy
 #from django.shortcuts import render
 
 
@@ -18,6 +19,18 @@ class BlogCreateView(CreateView):
     model = Post
     template_name = 'post_new.html'
     fields = '__all__'
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    fields = ['title', 'body']
+    template_name = 'post_edit.html'
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
 
 
 '''
